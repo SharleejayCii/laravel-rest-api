8bd7a49e78d3b68bf56c3170c396328d75f2c9e4
Carlos Jimenez <carlos.jimenez@jadelrio.com>
2021-10-11 15:12:11 -0500

    Agregar endPoint para crear una cuenta con un balance inicial

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function store(Request $request){
+        if($request->input('type')==='deposit'){
+            return $this->deposit(
+                $request->input('destination'),
+                $request->input('amounth')
+            );
+        }
+    }
+
+    private function deposit($destination, $amounth){
+        $account = Account::firstOrCreate([
+            'id' => $destination
+        ]);
+
+        $account->balance += $amounth;
+        $account->save();
+        return response()->json([
+            'destination' => [
+                'id' => $account->id,
+                'balance' => $account->balance
+            ]
+        ], 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\BalanceController;
 use App\Http\Controllers\ResetController;
+use App\Http\Controllers\EventController;
 
 use Illuminate\Support\Facades\Route;
 
 Route::post('/reset', [ResetController::class, 'reset']);
 
 Route::get('/balance',[BalanceController::class, 'show']);
+
+Route::post('/event', [EventController::class, 'store']);
